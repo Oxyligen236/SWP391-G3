8cae04a2c26a72cb59641c7cdfd2531cc939c86a
Byming1 <pqm1290@gmail.com>
2025-10-01 21:43:39 +0700

    LoginServlet

--- src/main/java/com/hrms/authentication/LoginServlet.java
+++ src/main/java/com/hrms/authentication/LoginServlet.java
@@ -9,18 +9,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
-public class Login extends HttpServlet {
+public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    // Hiển thị trang login khi truy cập GET
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Chuyển tiếp về trang login.jsp
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
-    // Xử lý form login khi người dùng POST
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
