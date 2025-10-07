3e17d3878a02e4178a153c9cc168f0db09104636
Byming1 <pqm1290@gmail.com>
2025-10-07 22:00:52 +0700

    Fix conflic

--- src/main/java/hrms/model/Account.java
+++ src/main/java/hrms/model/Account.java
@@ -1,34 +1,6 @@
 package hrms.model;
 
 public class Account {
-<<<<<<< HEAD
-    private int accountID;
-    private int userID;
-    private int roleID;
-    private String username;
-    private String password;
-    private boolean isActive;
-
-    public Account() {}
-
-    public int getAccountID() { return accountID; }
-    public void setAccountID(int accountID) { this.accountID = accountID; }
-
-    public int getUserID() { return userID; }
-    public void setUserID(int userID) { this.userID = userID; }
-
-    public int getRoleID() { return roleID; }
-    public void setRoleID(int roleID) { this.roleID = roleID; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public boolean isActive() { return isActive; }
-    public void setActive(boolean active) { isActive = active; }
-=======
 
     private int accountID;
     private int userID;
@@ -97,5 +69,4 @@ public class Account {
         this.isActive = isActive;
     }
 
->>>>>>> 10fe2e1d1659060b47d0e575b95baeb50b1f6f37
 }
