1df810309d924b7ea8eb8b379e0222c7573f9620
Luong Thi Van Ly <luongvanly133@gmail.com>
2025-10-26 16:06:29 +0700

    update userDao

--- src/main/java/hrms/model/Department.java
+++ src/main/java/hrms/model/Department.java
@@ -3,14 +3,17 @@ package hrms.model;
 public class Department {
     private int departmentId;
     private String name;
+    private int managerId; 
 
     public Department() {}
 
-    public Department(int departmentId, String name) {
+    public Department(int departmentId, String name, int managerId) {
         this.departmentId = departmentId;
         this.name = name;
+        this.managerId = managerId;
     }
 
+  
     public int getDepartmentId() {
         return departmentId;
     }
@@ -27,11 +30,20 @@ public class Department {
         this.name = name;
     }
 
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
     @Override
     public String toString() {
         return "Department{" +
                 "departmentId=" + departmentId +
                 ", name='" + name + '\'' +
+                ", managerId=" + managerId +
                 '}';
     }
 }
