b1af7686870410a3110106ff6511b912e55d3791
Byming1 <pqm1290@gmail.com>
2025-11-06 23:32:42 +0700

    sửa lại logic calendercheck

--- src/main/java/hrms/service/CalendarCheck.java
+++ src/main/java/hrms/service/CalendarCheck.java
@@ -2,15 +2,26 @@ package hrms.service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.List;
 
 import hrms.dao.HolidayCalendarDAO;
 import hrms.dao.HolidayDAO;
+import hrms.dao.PayrollPolicyDAO;
 import hrms.model.Holiday;
 import hrms.model.HolidayCalendar;
+import hrms.model.PayrollPolicy;
 
 public class CalendarCheck {
+
+    private HolidayDAO holidayDao;
+    private HolidayCalendarDAO calendarDao;
+    private PayrollPolicyDAO policyDAO;
+
+    public CalendarCheck() {
+        this.holidayDao = new HolidayDAO();
+        this.calendarDao = new HolidayCalendarDAO();
+        this.policyDAO = new PayrollPolicyDAO();
+    }
 
     public boolean checkHoliday(LocalDate date) {
         if (date == null) {
@@ -21,8 +32,6 @@ public class CalendarCheck {
         int month = date.getMonthValue();
         int year = date.getYear();
 
-        HolidayDAO holidayDao = new HolidayDAO();
-        HolidayCalendarDAO calendarDao = new HolidayCalendarDAO();
         HolidayCalendar calendar = calendarDao.getByYear(year);
 
         if (calendar == null) {
@@ -32,11 +41,11 @@ public class CalendarCheck {
         List<Holiday> holidays = holidayDao.getHolidaysByCalendar(calendar.getCalendarID());
 
         for (Holiday holiday : holidays) {
-            LocalDate holidayDate = holiday.getDateHoliday().toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDate();
+            LocalDate holidayDate = holiday.getDateHoliday();
+            int holidayDay = holidayDate.getDayOfMonth();
+            int holidayMonth = holidayDate.getMonthValue();
 
-            if (holidayDate.getDayOfMonth() == day && holidayDate.getMonthValue() == month) {
+            if (holidayDay == day && holidayMonth == month) {
                 return true;
             }
         }
@@ -50,6 +59,7 @@ public class CalendarCheck {
         }
 
         DayOfWeek dayOfWeek = date.getDayOfWeek();
+
         return (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY);
     }
 
@@ -72,8 +82,6 @@ public class CalendarCheck {
         int month = date.getMonthValue();
         int year = date.getYear();
 
-        HolidayDAO holidayDao = new HolidayDAO();
-        HolidayCalendarDAO calendarDao = new HolidayCalendarDAO();
         HolidayCalendar calendar = calendarDao.getByYear(year);
 
         if (calendar == null) {
@@ -83,15 +91,51 @@ public class CalendarCheck {
         List<Holiday> holidays = holidayDao.getHolidaysByCalendar(calendar.getCalendarID());
 
         for (Holiday holiday : holidays) {
-            LocalDate holidayDate = holiday.getDateHoliday().toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDate();
+            LocalDate holidayDate = holiday.getDateHoliday();
+            int holidayDay = holidayDate.getDayOfMonth();
+            int holidayMonth = holidayDate.getMonthValue();
 
-            if (holidayDate.getDayOfMonth() == day && holidayDate.getMonthValue() == month) {
+            if (holidayDay == day && holidayMonth == month) {
                 return holiday.isSubstitute();
             }
         }
 
         return false;
     }
+
+    public String getDayType(LocalDate date) {
+        if (date == null) {
+            return "Weekday";
+        }
+
+        if (checkHoliday(date) || checkSubstituteHoliday(date)) {
+            return "Holiday";
+        }
+
+        if (checkWeekend(date)) {
+            return "Weekend";
+        }
+
+        return "Weekday";
+    }
+
+    public double getOTSalaryPercentage(String dayType) {
+        String policyName = "";
+
+        if ("Holiday".equals(dayType)) {
+            policyName = "ot_multiplier_holiday";
+        } else if ("Weekend".equals(dayType)) {
+            policyName = "ot_multiplier_weekend";
+        } else {
+            policyName = "ot_multiplier_weekday";
+        }
+
+        PayrollPolicy policy = policyDAO.getByName(policyName);
+        if (policy != null) {
+            return policy.getPolicyValue();
+        }
+
+        return 150.0;
+    }
+
 }
